5a93a67279fac0fcc7f8281719a3cd0221117a40
honor2016tw <honor2016tw@gmail.com>
2021-08-26 17:12:48 +0800

    docker env fixed

--- env/docker/pwn/run.sh
+++ env/docker/pwn/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker build --no-cache -t pwn .
 
-docker run -it --rm --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v `pwd`:/root/ -w /root/ pwn
+docker run -it --rm --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v `pwd`:/tmp/host -w /root pwn
 
 # alias pwn = 'docker run -it --rm --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v `pwd`:/root/ -w /root/ pwn'
